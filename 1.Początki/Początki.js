0b7277adf04b251bcbd8839f50d3adb18146d436
wiktoriawlodek <61661388+wiktoriawlodek@users.noreply.github.com>
2021-02-04 16:58:20 +0100

    Add files via upload

---
+++ 1.Początki/Początki.js
@@ -0,0 +1,23 @@
+//ZADANIE 1. Wpisz w konsoli następującą instrukcję: console.log("Twoje imię");
+    
+    console.log("Wiktoria");  //Zwraca: "Wiktoria"
+
+    
+
+
+//ZADANIE 2. Wpisz w konsoli następującą instrukcję i sprawdź ich wynik: 
+
+
+    console.log(12 + 32); // Zwraca: suma, 44
+    console.log(52 / 16); // Zwraca: iloraz, zmiennoprzecinkwoa, 3.25
+    console.log(5 * 2); // Zwraca: iloczyn, 10
+
+
+
+// ZADANIE 3. Wpisz do konsoli i sprawdź co się wyświetli:
+
+    console.log("Hello World");   // Zwraca: "Hello World"
+    console.log(Hello World);    // Zwraca: Błąd, nieznana zmienna
+    
+    console.log("Hello World;   
+    //Zwraca: Błąd, brak znaku kończącego string
