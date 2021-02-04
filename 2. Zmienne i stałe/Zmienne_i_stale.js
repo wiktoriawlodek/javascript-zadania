05a29ad0d3ff9d9633ec2803cba39b9df7b5e967
wiktoriawlodek <61661388+wiktoriawlodek@users.noreply.github.com>
2021-02-04 17:00:04 +0100

    Add files via upload

---
+++ 2. Zmienne i stałe/Zmienne_i_stale.js
@@ -0,0 +1,34 @@
+/* ZADANIE 1. Stwórz w zmienne wstawiając do każdej z nich następujące dane i wyświetl je w konsoli:
+liczbę, string, liczbę + string np. (2 + "dwa"), wartość logiczną, wartość specjalną */
+
+
+let number = 5;
+let name = "Jacek";
+let mix = 2 + "dwa";
+let logical = false;
+let special = null;
+
+console.log(number);
+console.log(name);
+console.log(mix);
+console.log(logical);
+console.log(special);
+
+
+/* ZADANIE 2. W konsoli stwórz następujące zmienne:
+dwie zmienne mają przechowywać dowolne liczby, ostatnia o nazwie sum ma przechowywać liczbę 0. Następnie wypisZ w konsoli sumę dwóch pierwszych zmiennych przypisując wynik do zmiennej sum. */
+
+let nr1 = 5;
+let nr2 = 8;
+let sum = 0;
+
+console.log(sum = nr1 + nr2);
+
+
+/* ZADANIE 3. W konsoli stwórz zmienną i nic do niej nie przypisuj. Wypisz ją w konsoli i powiedz skąd taki wynik. */
+
+let emptyone;
+
+console.log(emptyone);
+
+// Zwraca: undefined - nie została przypisana żadna wartość.
